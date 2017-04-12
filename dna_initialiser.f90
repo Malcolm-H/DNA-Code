4d41bc937ca9728d93ed231c09030195e5c1e84d
Malcolm-H <malcolm.hillebrand@gmail.com>
2017-04-12 08:04:56 +0200

    update

--- dna_initialiser.f90
+++ dna_initialiser.f90
@@ -9,12 +9,12 @@ program dnao4
   integer, parameter :: rk = 8
   integer, parameter :: ik = 4, nini=21_ik, nfin=20_ik
   integer(ik), parameter :: ndof=100, ndof2=ndof*2
-  integer(ik) :: ii, jj, noc, acount, flag, recl=100
+  integer(ik) :: ii, jj, noc, acount, flag, recl=100, kk
   real(rk), dimension(ndof2) :: x, w, pr !x=(q1,q2,...,qN,p1,p2,...,pN)
   real(rk), dimension(ndof) :: atgc, atgc2, D, a, p0, xrange !at=0, gc=1
   real(rk), dimension(ndof,ndof) :: Dgc, Dat, agc, aat
   real(rk) :: pi=3.1415926535
-  real(rk) :: h, tf, E0, ma, kb, lambda, pat
+  real(rk) :: h, tf, E0, ma, kb, lambda, pat, t1 ,t2
   character(len=2*ndof) :: atgcchar
   character(len=ndof) :: at1, at2
   ! character(len=8) :: x1, fmt
@@ -41,52 +41,62 @@ program dnao4
   !!!
   call init_random_seed()
   !!! choose atgc randomly based on ndof, ngc
-  open(unit=122, file='../DNA-Data/ATGC_100_4.txt')
-  open(unit=123, status='scratch', access='direct', recl=200, action='readwrite')
-  acount = 0
-  do jj=1,10000
-    flag = 0
-    atgc = get_atgc(ndof, (ndof- int(pat * ndof)))
-    noc = 0_ik
-    do ii=1,ndof-1
-      if(atgc(ii) .ne. atgc(ii+1)) noc = noc + 1_ik
-    end do
-    if (atgc(1) .ne. atgc(ndof)) noc = noc + 1_ik
-    ! this and the two loops create the concatenated string to be checked against
-    !-----------------------------------------------------
-    atgcchar = char(int(atgc(1)))
-    do ii=2,10
-      atgcchar = trim(atgcchar) // trim(char(int(atgc(ii) + 48)))
-    end do
-    do ii=1,10
-      atgcchar = trim(atgcchar) // trim(char(int(atgc(ii) + 48)))
-    end do
-    !-----------------------------------------------------
-
-    ! loop and read recorded atgc values to compare against
-    at2 = char(int(atgc(1) + 48))
-    do ii=2,ndof
-      at2 = trim(at2) // trim(char(int(atgc(ii) + 48)))
-    end do
-    if (jj .gt. 1) then
-      do ii = 1,acount
-        read(unit=123,rec=ii) at1
-        atgcchar = trim(at1) // trim(at1)
-        if (index(atgcchar, at2) .ne. 0) then
-          flag = 1
-          exit
-        end if
+  open(unit=122, file='../DNA-Data/ATGC_100_n500000.txt')
+  ! open(unit=121, file='../DNA-Data/time_noc.txt')
+  ! do kk=50,3000,50
+    acount = 0
+    open(unit=123, status='scratch', access='direct', recl=200, action='readwrite')
+    ! call cpu_time(t1)
+    do jj=1,500000
+      flag = 0
+      atgc = get_atgc(ndof, (ndof- int(pat * ndof)))
+      noc = 0_ik
+      do ii=1,ndof-1
+        if(atgc(ii) .ne. atgc(ii+1)) noc = noc + 1_ik
       end do
-    end if
-    if (flag .eq. 0) then
-      write(unit=123,rec=jj) at2
-      write(122,*) at2
-      acount = acount + 1
-    end if
-    atgc = 0._rk
-  end do
+      if (atgc(1) .ne. atgc(ndof)) noc = noc + 1_ik
+      ! this and the two loops create the concatenated string to be checked against
+      !-----------------------------------------------------
+      atgcchar = char(int(atgc(1)+48))
+      do ii=2,10
+        atgcchar = trim(atgcchar) // trim(char(int(atgc(ii) + 48)))
+      end do
+      do ii=1,10
+        atgcchar = trim(atgcchar) // trim(char(int(atgc(ii) + 48)))
+      end do
+      !-----------------------------------------------------
+      ! loop and read recorded atgc values to compare against
+      at2 = char(int(atgc(1) + 48))
+      do ii=2,ndof
+        at2 = trim(at2) // trim(char(int(atgc(ii) + 48)))
+      end do
+      if (jj .gt. 1) then
+        do ii = 1,acount
+          read(unit=123,rec=ii) at1
+          atgcchar = trim(at1) // trim(at1)
+
+          if (index(atgcchar, at2) .ne. 0) then
+            flag = 1
+            print*, 1
+            exit
+          end if
+        end do
+      end if
+      !-----------------------------------------------------
+      if (flag .eq. 0) then
+        write(unit=123,rec=jj) at2
+        write(122,*) at2
+        acount = acount + 1
+      end if
+      atgc = 0._rk
+    end do
+    ! call cpu_time(t2)
+    ! write(121,*) kk, ', ', (t2-t1)
+    close(123)
+
+  ! end do
+  ! close(121)
   close(122)
-  close(123)
   ! open(unit=99,file='../DNA-Data/ATGC_10_4.txt')
 
   !!!
